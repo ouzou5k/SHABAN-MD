87bbbb4be720ffcf8ce933878348ee79e65e4d24
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-26 07:10:15 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,81 +2,83 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+return text === fault ? true : false;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
+// add your Session Id
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
+// make true if you want auto reply on status
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// make true if you want auto react on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW 𓅓 Oᴜᴢᴏᴜ_officiel*",
+// make true if you want auto reply on status
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW Sʜᴀʙᴀɴ-Mᴅ 𓅓",
 // set the auto reply massage on status reply
 WELCOME: process.env.WELCOME || "false",
-// true if want welcome and goodbye msg in groups    
+// true if want welcome and goodbye msg in groups
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group  
-PREFIX: process.env.PREFIX || ":",
+// make true to know who dismiss or promoted a member in group
+PREFIX: process.env.PREFIX || ">",
 // add your prifix for bot
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
-// add custom menu and mention reply image url   
-BOT_NAME: process.env.BOT_NAME || "Ouzou_officiel-MD",
-// add bot name here for menu
-STICKER_NAME: process.env.STICKER_NAME || "Ouzou_officiel",
-// type sticker pack name 
+// add custom menu and mention reply image url
+BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
+// type sticker pack name
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custom emoji react    
+// make this true for custum emoji react
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍,😝,😂",
-// chose custom react emojis by yourself 
+// chose custom react emojis by yourself
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links without remove member 
+// automatic delete links witho remove member
 OWNER_NUMBER: process.env.OWNER_NUMBER || "22383267744",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Ouzou_officiel",
+OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Oᴜᴢᴏᴜ_officiel*",
-// add bot owner name    
+DESCRIPTION: process.env.DESCRIPTION || "© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ",
+// add bot owner name
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *Ouzou_officiel-MD*⚡",
-// add alive msg here 
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar SHABAN-MD⚡",
+// add alive msg here
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
+// false or true for anti bad words
 MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
+// make bot public-private-inbox-group
 ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
+// make anti link true,false for groups
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
+// make true for automatic stickers
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
+// make true or false automatic text reply
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-// maks true for always online 
+// maks true for always online
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing   
+// true for automatic show typing
 READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
+// true if want mark commands as read
 DEV: process.env.DEV || "22383267744",
-//replace with your whatsapp number        
+//replace with your whatsapp number
 ANTI_VV: process.env.ANTI_VV || "false",
 // true for anti once view
 ANTI_CALL: process.env.ANTI_CALL || "false",
-REJECT_MSG: process.env.REJECT_MSG || "*_SOORY MY BOSS (Ouzou_officiel) IS BUSY PLEASE DONT CALL ME_*",
+REJECT_MSG: process.env.REJECT_MSG || "SOORY MY BOSS IS BUSY PLEASE DONT CALL ME",
 ANTI_DELETE: process.env.ANTI_DELETE || "false",
 // set true false for anti delete
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
 // change it to 'same' if you want to resend deleted message in same chat
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
+// make it true for auto recoding
 };
+Modifie certaines parties écris mon nom Ouzou_officiel la ba
+
